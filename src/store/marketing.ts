4b0897387664d510a32954ae1b85224b4b55ea30
Jared Jolton <jared.jolton@gmail.com>
2022-01-13 23:25:16 -0700

    make marketing way more expensive, change earth2 map

--- src/store/marketing.ts
+++ src/store/marketing.ts
@@ -2,22 +2,22 @@ export const marketingProjects: MarketingProject[] = [
   {
     name: 'throw up some fliers',
     successMessage: 'you put the word out',
-    cost: 0.000000075
+    cost: 0.00075
   }, {
     name: 'make a social media account',
     successMessage: 'does anyone even use myspace anymore',
-    cost: 0.00000015
+    cost: 0.0015
   }, {
     name: 'advertise in the local newspaper',
     successMessage: 'all cops read the newspaper',
-    cost: 0.000001
+    cost: 0.01
   }, {
     name: 'advertise on the local news',
     successMessage: 'donuts look good on tv',
-    cost: 0.00003
+    cost: 0.3
   }, {
     name: 'make more social media accounts',
     successMessage: 'we\'re gonna go viral with this one',
-    cost: 0.00005
+    cost: 0.5
   }
 ]
